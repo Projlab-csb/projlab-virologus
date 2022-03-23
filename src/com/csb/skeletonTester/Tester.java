8cc89eaf5534f98405ed02512203f719023900a2
Kostyal Balint <kostyal.balintnak@gmail.com>
2022-03-23 18:53:23 +0100

    Tester + UserInputHandler

--- src/com/csb/skeletonTester/Tester.java
+++ src/com/csb/skeletonTester/Tester.java
@@ -1,7 +1,27 @@
 package com.csb.skeletonTester;
 
 public class Tester {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private Tester _tester;
+    private int indentationCount;
+
+    public Tester() {
+        int indentationCount = 0;
+    }
+
+    public Tester getInstance(){
+        if(_tester == null){
+            _tester = new Tester();
+        }
+        return _tester;
+    }
+
+    public void functionStart(String functionName){
+        System.out.println("\t".repeat(indentationCount) + "--> Function Start: " + functionName);
+        indentationCount++;
+    }
+
+    public void FunctionEnd(String functionName){
+        System.out.println("\t".repeat(indentationCount) + "--> Function End: " + functionName);
+        indentationCount--;
     }
 }
---
+++ src/com/csb/skeletonTester/UserInputHandler.java
@@ -0,0 +1,55 @@
+package com.csb.skeletonTester;
+
+public class UserInputHandler {
+
+    private static UserInputHandler _instance;
+
+    private UserInputHandler() {
+    }
+
+    public static UserInputHandler getInstance() {
+        if (_instance == null) {
+            _instance = new UserInputHandler();
+        }
+        return _instance;
+    }
+
+    public int getUserInputInt(String prompt) {
+        System.out.print(prompt);
+        return Integer.parseInt(System.console().readLine());
+    }
+
+    public String getUserInputString(String prompt) {
+        System.out.print("[UserInput String]" + prompt);
+        return System.console().readLine();
+    }
+
+    public String getUserInputString(String prompt, String[] options) {
+        System.out.print("[UserInput String]" + prompt + "(" + String.join(", ", options) + "): ");
+        String userInput = System.console().readLine();
+        while (!isValidOption(userInput, options)) {
+            System.out.print("Invalid input. Please try again: ");
+            userInput = System.console().readLine();
+        }
+        return userInput;
+    }
+
+    private boolean isValidOption(String userInput, String[] options) {
+        for (String option : options) {
+            if (option.equals(userInput)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean getUserInputBoolean(String prompt) {
+        System.out.print("[UserInput Boolean]" + prompt + " (y/n): ");
+        String userInput = System.console().readLine();
+        while (!isValidOption(userInput, new String[] {"y", "n"})) {
+            System.out.print("Invalid input. Please try again: ");
+            userInput = System.console().readLine();
+        }
+        return userInput.equals("y");
+    }
+}
