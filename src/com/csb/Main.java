d59de30ba9673a13f8dade0d15ba0fecf5095e22
Oliver Raisz <raiszoliver@gmail.com>
2022-04-24 10:18:28 +0200

    main function update

--- src/com/csb/Main.java
+++ src/com/csb/Main.java
@@ -2,8 +2,11 @@ package com.csb;
 
 import static com.csb.utils.ClassLoader.findAllClassesUsingClassLoader;
 
+import com.csb.gameControl.GameController;
 import com.csb.skeletonTester.TestInterface;
 import com.csb.skeletonTester.Tester;
+import com.csb.skeletonTester.Tests.StealCloak;
+
 import java.text.ParseException;
 import java.util.Collections;
 import java.util.Comparator;
@@ -12,15 +15,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    /**
-     * This is the main method. It is the entry point of the program. It is the first method called when the program
-     * is run. It handles the test selection and the test execution. First all the test classes are loaded using the
-     * class loader. Then the user is asked to select a test to run. The test is then executed. This is done until the
-     * user enters "-1" as the test id, which is the exit command.
-     *
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+    static void skeletonTest(){
         // User input scanner instance
         Scanner scanner = new Scanner(System.in);
 
@@ -70,7 +65,48 @@ public class Main {
             } catch (NumberFormatException e) {
                 System.out.println("Invalid input (try to enter a number): " + e.getMessage());
             } catch (Exception e) {
-                e.printStackTrace(); 
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * This is the main method. It is the entry point of the program. It is the first method called when the program
+     * is run. It handles the test selection and the test execution. First all the test classes are loaded using the
+     * class loader. Then the user is asked to select a test to run. The test is then executed. This is done until the
+     * user enters "-1" as the test id, which is the exit command.
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("What would you like to run? Enter the number of the option from below:");
+        System.out.println("----------------------------------------------------------");
+        System.out.println("1 - Skeleton tests");
+        System.out.println("2 - Integration tests");
+        System.out.println("3 - Game");
+
+        Scanner scanner = new Scanner(System.in);
+        int input = -1;
+        while (input < 0 || input > 3) {
+            try {
+                input = Integer.parseInt(scanner.nextLine());
+                switch (input) {
+                    case 1:
+                        skeletonTest();
+                        break;
+                    case 2:
+                        //TODO write integration tests
+                        break;
+                    case 3:
+                        GameController.getInstance().initGame();
+                        GameController.getInstance().startGame();
+                        break;
+                    default:
+                        System.out.println("Only numbers between 1 and 3 are valid, try again");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input (try to enter a number): " + e.getMessage());
+                input = -1;
             }
         }
     }
